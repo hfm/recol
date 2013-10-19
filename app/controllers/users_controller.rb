486007a13bc2512e5b5a559c08993e1e70b31ebd
putchom <putchom8@gmail.com>
2013-10-19 21:10:57 +0900

    users#createアクションを作って、ユーザーをDBに保存した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
 end
