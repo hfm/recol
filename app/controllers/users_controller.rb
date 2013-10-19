eac5d26647eb4a8662af7e601b7c70eb66377f8f
putchom <putchom8@gmail.com>
2013-10-19 21:15:03 +0900

    strong parametersを設定した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,9 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
 end
