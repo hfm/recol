4f7e94527b01a8b512292aaac1bf8b80e6c004f6
putchom <putchom8@gmail.com>
2013-10-19 20:45:21 +0900

    ルーティングでusersを設定した

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Recol::Application.routes.draw do
   get "users/show"
   get "users/new"
   get "users/edit"
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
