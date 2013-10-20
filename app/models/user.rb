545594a05c31713764f0f25f93203f38d489afe1
putchom <putchom8@gmail.com>
2013-10-20 13:05:10 +0900

    ユーザー登録にvalidationをつけた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
 end
